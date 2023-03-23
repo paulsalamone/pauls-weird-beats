b9766841a409f71bc960793084b823609094c024
Paul Salamone <93609282+paulsalamone@users.noreply.github.com>
2023-03-24 00:23:57 +0100

    connected to api

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,36 @@
       <TopNav />
     </div>
   </header>
-  <main><RouterView /></main>
+  <main>
+    <RouterView />
+
+    DATA: {{ data }}
+  </main>
 </template>
 
 <script setup>
-import { RouterView } from 'vue-router'
+// import { RouterView } from 'vue-router'
 import TopNav from './components/TopNav.vue'
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const data = ref([])
+const URL = 'https://api.gumroad.com/v2/products'
+const ACCESS_TOKEN = import.meta.env.VITE_ACCESS_TOKEN
+console.log(ACCESS_TOKEN)
+onMounted(async () => {
+  try {
+    const response = await axios.get(URL, {
+      headers: {
+        Authorization: `Bearer ${ACCESS_TOKEN}`
+      }
+    })
+
+    data.value = response.data
+  } catch (error) {
+    console.error(error)
+  }
+})
 </script>
 
 <style scoped>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,20 @@
 <template>
   <main>
     <h1 v-html="title"></h1>
-    <!-- <h2>{{ arr }}</h2> -->
-    <!-- <h2>{{ output }}</h2> -->
-
     <h3>Coming soon...</h3>
   </main>
 </template>
 
 
 <script setup>
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 
 const title = ref('')
-
 const titleOrig = "Paul's Weird Beats"
 const arr = titleOrig.split('')
 arr.map((e) => {
   title.value += `<span style="font-size: ${r()}px">${e}</span>`
 })
-
-console.log(title.value)
 
 function r() {
   return Math.floor(Math.random() * 40) + 40
