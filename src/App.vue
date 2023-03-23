ede0984f8e40388b639fd884c68d7111f454d16f
Paul Salamone <93609282+paulsalamone@users.noreply.github.com>
2023-03-23 23:08:40 +0100

    first commit!

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <TopNav />
+    </div>
+  </header>
+  <main><RouterView /></main>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+import TopNav from './components/TopNav.vue'
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/TopNav.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav class="b-nav">
+    <RouterLink class="b-nav__link" to="/">Home</RouterLink>
+    <RouterLink class="b-nav__link" to="/about">About</RouterLink>
+    <RouterLink class="b-nav__link" to="/shop">Shop</RouterLink>
+    <RouterLink class="b-nav__link" to="/shop">Your Cart</RouterLink>
+    <RouterLink class="b-nav__link" to="/shop">Contact</RouterLink>
+  </nav>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+import { ref } from 'vue'
+</script>
+
+<style>
+.b-nav__link {
+  color: red;
+  margin: 2rem;
+}
+</style>
